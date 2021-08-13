122f14d0726cff6882795e822eae763db913048c
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-08-12 20:49:43 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,25 @@
+//get values from the interface
+function getValues(){
+    //get values from the page
+        let startValue = document.getElementById('startValue').value;
+        let endValue = document.getElementById('endValue').value;
+        alert("the start value: " + startValue);
+
+    //call generate numbers
+
+
+    //call display numbers
+
+}
+//gererate numbers from start value to end value
+function generateNumbers(start, end){
+
+    for(let i=start; i<=end; i++){
+        
+    }
+}
+
+//display numbers and mark even numbers bold
+function displayNumbers(){
+
+}

2c7669fa6fcbb21942d526be4bd22352cc308848
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-08-14 10:18:02 -0400

    finished 0-100 app

--- js/site.js
+++ js/site.js
@@ -3,9 +3,18 @@ function getValues(){
     //get values from the page
         let startValue = document.getElementById('startValue').value;
         let endValue = document.getElementById('endValue').value;
-        
+        let numbers = [];
 
-    //call generate numbers
+        startValue = parseInt(startValue);
+        endValue = parseInt(endValue);
+        
+        if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+         let numbers =  generateNumbers(startValue,endValue);
+         displayNumbers(numbers);
+        }else{
+            alert("You must enter an integer");
+        }
+            //call generate numbers
 
 
     //call display numbers
@@ -13,13 +22,25 @@ function getValues(){
 }
 //gererate numbers from start value to end value
 function generateNumbers(start, end){
-let numbers = [];
+ let numbers = [];
     for(let i=start; i<=end; i++){
         numbers.push(i);
     }
+    return numbers;
 }
 
 //display numbers and mark even numbers bold
-function displayNumbers(){
-
+function displayNumbers(numbers){
+    let templateRows = "";
+for (let index = 0; index < numbers.length; index++) {
+    let number = numbers[index];
+    if(number % 2 == 0){
+        className="even";
+    }else{
+        className="odd";
+    }
+    templateRows += `<tr><td class="${className}">${number}</td></tr>`;   
+    
+}
+    document.getElementById("results").innerHTML = templateRows;
 }
